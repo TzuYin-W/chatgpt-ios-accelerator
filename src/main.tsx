27184ff43d1b1cd7ac579a74a635cc213db0ca9b
TzuYin-W <wtzuyin@gmail.com>
2025-07-23 00:53:11 +0800

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChatGPTiOSAccelerator from "./ChatGPTiOSAccelerator";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChatGPTiOSAccelerator />
+  </React.StrictMode>
+);
